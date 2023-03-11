8c6ed6ab96728792c83f4fb0f715f74143356445
Ivan Murzin <vanya.murzin@inbox.ru>
2023-03-11 15:02:20 +0500

    created removeUser method, error occurs

--- Codes/Payments.py
+++ Codes/Payments.py
@@ -9,6 +9,7 @@ class Payments(Window):
         self.form.backToMenu.clicked.connect(self.showBackToMenu)
         self.form.enterid.textChanged.connect(self.search)
         self.form.addButton.clicked.connect(self.addUser)
+        self.form.removeButton.clicked.connect(self.removeUser)
 
     def showBackToMenu(self):
         Window.windows['Menu']['window'].show()
@@ -34,8 +35,7 @@ class Payments(Window):
         try:
             profit = float(self.form.resultProfit.text().split(' ')[0])
             if self.form.searchTable.currentItem() is None:
-                raise Exception ('Select id')
-
+                raise Exception('Select id')
 
             if self.form.searchTable.currentColumn() == 1:
                 id, username, payment = Window.db.getPaymentInfo(username=self.form.searchTable.currentItem().text())
@@ -43,9 +43,9 @@ class Payments(Window):
                 id, username, payment = Window.db.getPaymentInfo(id=self.form.searchTable.currentItem().text())
             else:
                 raise Exception('Select user first')
-            row_count=self.form.resultTable.rowCount()
+            row_count = self.form.resultTable.rowCount()
             for i in range(row_count):
-                if id == int(self.form.resultTable.item(i,0).text()):
+                if id == int(self.form.resultTable.item(i, 0).text()):
                     raise Exception('User already in list')
 
             self.form.resultTable.setRowCount(row_count + 1)
@@ -53,8 +53,8 @@ class Payments(Window):
             if username is not None:
                 self.form.resultTable.setItem(row_count, 1, QTableWidgetItem(username))
             self.form.resultTable.setItem(row_count, 2, QTableWidgetItem(str(payment)))
-            profit+=payment
-            self.form.resultProfit.setText(str(profit)+' р')
+            profit += payment
+            self.form.resultProfit.setText(str(profit) + ' р')
 
         except Exception as e:
             dlg = QMessageBox()
@@ -64,3 +64,33 @@ class Payments(Window):
             dlg.setIcon(QMessageBox.Icon.Warning)
             dlg.exec()
 
+    def removeUser(self):
+        try:
+            profit = float(self.form.resultProfit.text().split(' ')[0])
+            if self.form.resultTable.currentItem() is None:
+                raise Exception('Select id')
+
+            if self.form.resultTable.currentColumn() == 1:
+                id, username, payment = Window.db.getPaymentInfo(username=self.form.resultTable.currentItem().text())
+            elif self.form.resultTable.currentColumn() == 0:
+                id, username, payment = Window.db.getPaymentInfo(id=self.form.resultTable.currentItem().text())
+            elif self.form.resultTable.currentColumn() == 2:
+                raise Exception("Select user, not the payment value")
+            else:
+                raise Exception("Select user first")
+
+            row_count = self.form.resultTable.rowCount()
+            for row in range(row_count):
+                if id == int(self.form.resultTable.item(row, 0).text()):
+                    self.form.resultTable.removeRow(row)
+
+            profit -= payment
+            self.form.resultProfit.setText(str(profit) + ' р')
+
+        except Exception as e:
+            dlg = QMessageBox()
+            dlg.setWindowTitle("Error")
+            dlg.setText(str(e))
+            dlg.setStandardButtons(QMessageBox.StandardButton.Ok)
+            dlg.setIcon(QMessageBox.Icon.Warning)
+            dlg.exec()
