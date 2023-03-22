959035e8c5650d12332cfda6e618184c3676e377
Ivan Murzin <vanya.murzin@inbox.ru>
2023-03-22 15:17:50 +0500

    Created self-show function if 'by month'is toggled on

--- Codes/OfficeHours.py
+++ Codes/OfficeHours.py
@@ -8,9 +8,27 @@ class OfficeHours(Window):
     def __init__(self):
         super().__init__('OfficeHours', r'Designs\OfficeHours.ui')
         self.form.backToMenu.clicked.connect(self.showBackToMenu)
+        self.form.byMonthButton.toggled.connect(self.byMonthPressed)
+        self.form.allButton.toggled.connect(self.allPressed)
         for teacher in self.db.getTeachers():
             self.form.teacherList.addItem(teacher)
-        if int(self.form.yearList.itemText(0))!=datetime.datetime.now().year
+        for i in reversed(range(2022, datetime.datetime.now().year+1)):
+            self.form.yearList.addItem(str(i))
+        self.form.monthList.hide()
+        self.form.yearList.hide()
+
+
+
+    def byMonthPressed(self):
+        self.form.monthList.show()
+        self.form.yearList.show()
+    def allPressed(self):
+        self.form.monthList.hide()
+        self.form.yearList.hide()
+
+
+
+
 
 
 
